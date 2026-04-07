f1eaca84b92621dd45f68d3b38bd8fbdad84ed0f
bukanberuangsr <akhfizpuli@gmail.com>
2026-04-07 12:59:02 +0700

    add route for users, personalization, recommendation, analytics, and segments

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,13 +16,61 @@ func main() {
 		})
 	})
 
-	// Rute otentikasi
-
-	auth := router.Group("api/auth")
+	/*
+		 * Rute otentikasi
+			* Autentikasi pengguna disarankan menggunakan JWTAUTH.
+		 	* Untuk enpointnya nunggu PM (Vina)
+			* Sementara cocokan dengan endpoint yang ada dibuat
+	*/
+	auth := router.Group("/api/auth")
 	{
 		auth.POST("/register", controller.Register)
 		auth.POST("/login", controller.Login)
 	}
 
-	router.Run(":8000")
+	/*
+		 * Rute user
+			* Berkaitan dengan aktifitas user seperti
+			* ambil data profil, ambil data transaksi,
+			* dan segment
+	*/
+	users := router.Group("/api/users")
+	{
+		users.GET("/:id")
+		users.GET("/:id/activity")
+		users.GET("/:id/segment")
+	}
+
+	/*
+		 * Rute personalisasi dan rekomendasi
+			* konfigurasi pengguna terkait personalisasi
+			* konten/fitur dan rekomendasi konten/fitur
+			* ke pengguna
+	*/
+	personalization := router.Group("/api/personalization")
+	{
+		personalization.GET("/:id")
+	}
+
+	recommendation := router.Group("/api/recommendation")
+	{
+		recommendation.GET("/:id")
+	}
+
+	/*
+		 * Rute analytics
+			* menyimpan analytics semua aktifitas dalam
+			* sebuah file/database (misalnya mongodb)
+	*/
+	analytics := router.Group("/api/analytics")
+	{
+		analytics.GET("/metrics") // TODO: protect with auth
+		analytics.POST("/event")
+	}
+
+	router.POST("/api/segments/update", func(ctx *gin.Context) {
+		// TODO: update segmentasi dari divisi AI/ML
+	})
+
+	router.Run(":8080")
 }
