b0dc50f37803ac1830408da9b245fad47b389f69
bukanberuangsr <akhfizpuli@gmail.com>
2026-04-21 19:11:28 +0700

    modify user struct to customer structs

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,16 @@
 package models
 
-type User struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+type Customer struct {
+	ID         int    `json:"id"`
+	CustomerID string `json:"customer_id"`
+	Username   string `json:"username"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
 }
 
-var Users = []User{}
+type CustomerProfile struct {
+	ID         int    `json:"id"`
+	CustomerID int    `json:"customer_id"` // Simpan ID (int) dari tabel Customer
+	Username   string `json:"username"`
+	FullName   string `json:"full_name"`
+}
